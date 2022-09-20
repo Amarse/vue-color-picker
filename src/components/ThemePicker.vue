4b2118ae899adbd124e37cb46a59b504cf16bfc0
Amarse <byicy08@gmail.com>
2022-09-20 16:31:03 +0900

    add ThemePicker.vue

---
+++ src/components/ThemePicker.vue
@@ -0,0 +1,24 @@
+<template>
+  <chrome-color-picker :value="themeColor" @input="setThemeColor($event.hex)" :desableAlpha="true"></chrome-color-picker>
+</template>
+
+<script>
+import { Chrome } from 'vue-color';
+import { mapGetters, mapMutations } from 'vuex';
+
+export default {
+  components: {
+    'chrome-color-picker': Chrome,
+  },
+
+  computed: {
+    ...mapGetters(['themeColor']),
+  },
+
+  methods: {
+    ...mapMutations(['setThemeColor']),
+  },
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,20 @@
 import Vue from "vue";
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const state = {
+  themeColor: '#41B883'
+};
+
+const mutations = {
+  setThemeColor(state, color) {
+    state.themeColor = color;
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  getters,
+});
